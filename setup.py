d300200d5efc1c629afea3617a779b3746106561
Alex Grachev <alxgrh@protonmail.ch>
2019-05-21 21:36:43 +0300

    add find_packages() to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='pyimgsaliency',
       version='0.1',
@@ -7,5 +7,5 @@ setup(name='pyimgsaliency',
       author='Yann Henon',
       author_email='none',
       license='Apache',
-      packages=['pyimgsaliency'],
+      packages=find_packages(),
       zip_safe=False)
